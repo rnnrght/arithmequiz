9f53d70bbb254d7aa3df99adc8592bcd80ecfe5e
Aaron Enright <aje@fact50.net>
2014-03-21 19:31:13 -0500

    a little more

--- arithmequiz.py
+++ arithmequiz.py
@@ -1,26 +1,33 @@
 import random
 
 class problem():
-	def __init__(self):
-		self.a=random.randrange(0,10)
-		self.b=random.randrange(0,10)
-	def printproblem(self, oper):
-		if oper=="addition":
-			print("%d + %d" %(self.a, self.b))
-			self.answer=self.a + self.b
-		if oper=="subtraction":
-			print("%d - %d" %(self.a, self.b))
-			self.answer=self.a - self.b
-	def getanswer(self):
-		self.ans=input(" = ")
-		if self.ans==self.answer:
-			print("correct!")
-		else:
-			print("wrong")
+    def __init__(self):
+        self.a=random.randrange(0,10)
+        self.b=random.randrange(0,10)
+    def getoper(self):
+        self.op=raw_input("enter + or - ")
+        if self.op=="+" or self.op=="-": return(self.op)
+        else: self.getoper()
+    def printproblem(self, oper):
+        if oper=="+":
+            print("%d + %d" %(self.a, self.b))
+            self.answer=self.a + self.b
+        if oper=="-":
+            print("%d - %d" %(self.a, self.b))
+            self.answer=self.a - self.b
+    def getanswer(self):
+        self.ans=input(" = ")
+        if self.ans==self.answer:
+            print("correct!")
+        else:
+            print("wrong")
 
 def cycle():
-	prob=problem()
-	prob.printproblem("addition")
-	prob.getanswer()
+    prob=problem()
+    prob.printproblem(prob.getoper())
+    prob.getanswer()
+
+if __name__ == '__main__':
+    cycle()
 
 
