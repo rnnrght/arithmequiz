384ef5a38771bd9ebdcbf2e1eaecf7dc98c03278
Aaron J. Enright <aje@fact50.net>
2014-04-15 18:54:35 -0500

    added docstrings

--- arithmequiz.py
+++ arithmequiz.py
@@ -1,14 +1,17 @@
 import random
 
 class problem():
+    """class for an arithmetic problem"""
     def __init__(self):
         self.a=random.randrange(0,10)
         self.b=random.randrange(0,10)
     def getoper(self):
+        """ask the user for an operand"""
         self.op=raw_input("enter + or - ")
         if self.op=="+" or self.op=="-": return(self.op)
         else: self.getoper()
     def printproblem(self, oper):
+        """print the problem"""
         if oper=="+":
             print("%d + %d" %(self.a, self.b))
             self.answer=self.a + self.b
@@ -16,6 +19,7 @@ class problem():
             print("%d - %d" %(self.a, self.b))
             self.answer=self.a - self.b
     def getanswer(self):
+        """get answer from the user and check it"""
         self.ans=input(" = ")
         if self.ans==self.answer:
             print("correct!")
@@ -23,6 +27,7 @@ class problem():
             print("wrong")
 
 def cycle():
+    """creates a problem object, prints the problem, and gets the answer"""
     prob=problem()
     prob.printproblem(prob.getoper())
     prob.getanswer()
