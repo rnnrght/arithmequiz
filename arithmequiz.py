d7b24b04513e585c3b493085884045998bec6d91
Aaron J. Enright <aje@fact50.net>
2014-04-15 19:43:27 -0500

    added multiplication quizzes can now be more than one problem long score is kept

--- arithmequiz.py
+++ arithmequiz.py
@@ -7,8 +7,8 @@ class problem():
         self.b=random.randrange(0,10)
     def getoper(self):
         """ask the user for an operand"""
-        self.op=raw_input("enter + or - ")
-        if self.op=="+" or self.op=="-": return(self.op)
+        self.op=raw_input("enter +, -, or * :")
+        if self.op=="+" or self.op=="-" or self.op=="*": return(self.op)
         else: self.getoper()
     def printproblem(self, oper):
         """print the problem"""
@@ -18,28 +18,38 @@ class problem():
         if oper=="-":
             print("%d - %d" %(self.a, self.b))
             self.answer=self.a - self.b
+        if oper=="*":
+            print("%d * %d" %(self.a, self.b))
+            self.answer=self.a * self.b
     def getanswer(self):
         """get answer from the user and check it"""
         self.ans=input(" = ")
         if self.ans==self.answer:
-            print("correct!")
+            return(True)
         else:
-            print("wrong")
+            return(False)
+
+class problemset():
+    """represents a set of problems"""
+    def __init__(self):
+        self.problemnumbers=input("How many problems would you like? ")
+        self.score=0
+    def doit(self):
+        for i in range(self.problemnumbers): 
+            self.res=cycle()
+            if self.res: self.score +=1
+            print(self.score)
+    def results(self): print("your score is %d right out of %d" %(self.score, self.problemnumbers))
 
 def cycle():
     """creates a problem object, prints the problem, and gets the answer"""
     prob=problem()
     prob.printproblem(prob.getoper())
-    prob.getanswer()
+    return(prob.getanswer())
 
-#TODO: multiplication
 #TODO: division (with remainders)
-#TODO: ask multiple questions
-#TODO: keep score
 #TODO: keep score history
 #TODO: have different player profiles
 
 if __name__ == '__main__':
     cycle()
-
-
